7eb77177d2f96e71b485be154264bdfabed3b737
Hernando Abella <hernandoabella@gmail.com>
2023-09-16 23:45:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const textElement = document.getElementById("logo");
+const container = document.querySelector(".container");
+const logo = document.querySelector(".logo");
 let colorIndex = 0;
-const colors = ["red", "green", "blue"]; // Puedes agregar más colores según tus preferencias
+const colors = ["red", "green", "blue", "orange", "purple"]; // Puedes agregar más colores según tus preferencias
 
-function changeColor() {
-  textElement.style.color = colors[colorIndex];
+function changeColors() {
+  logo.style.color = colors[colorIndex];
+  container.style.color = colors[colorIndex];
   colorIndex = (colorIndex + 1) % colors.length;
 }
 
-// Cambia el color cada 1000 milisegundos (1 segundo)
-setInterval(changeColor, 100);
+// Cambia los colores cada 2 segundos (2000 milisegundos)
+setInterval(changeColors, 2000);
