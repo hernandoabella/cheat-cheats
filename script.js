44663910da397fae36ce7e8739b010287cdfa29e
Hernando Abella <hernandoabella@gmail.com>
2023-09-17 01:57:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function changeColors() {
 
 // Cambia los colores cada 2 segundos (2000 milisegundos)
 setInterval(changeColors, 2000);
+
+// Función para activar o desactivar el modo oscuro
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  
+    // Guardar el estado del modo oscuro en localStorage
+    if (element.classList.contains("dark-mode")) {
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      localStorage.setItem("dark-mode", "disabled");
+    }
+  }
+  
+  // Verificar el estado del modo oscuro en localStorage al cargar la página
+  window.addEventListener("DOMContentLoaded", function () {
+    var darkModeState = localStorage.getItem("dark-mode");
+    if (darkModeState === "enabled") {
+      document.body.classList.add("dark-mode");
+    }
+  });
+  
+  // Llamar a toggleDarkMode() cuando se haga clic en un botón o elemento de cambio
+  document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
+  
