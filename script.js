83bbcf9dc73ec8de9bebec205a3a6dd6e6e669a7
Hernando Abella <hernandoabella@gmail.com>
2023-09-17 20:32:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const colors = ["red", "green", "blue", "orange", "purple"]; // Puedes agregar m
 // function toggleDarkMode() {
 //     var element = document.body;
 //     element.classList.toggle("dark-mode");
-  
+
 //     // Guardar el estado del modo oscuro en localStorage
 //     if (element.classList.contains("dark-mode")) {
 //       localStorage.setItem("dark-mode", "enabled");
@@ -24,7 +24,7 @@ const colors = ["red", "green", "blue", "orange", "purple"]; // Puedes agregar m
 //       localStorage.setItem("dark-mode", "disabled");
 //     }
 //   }
-  
+
 //   // Verificar el estado del modo oscuro en localStorage al cargar la página
 //   window.addEventListener("DOMContentLoaded", function () {
 //     var darkModeState = localStorage.getItem("dark-mode");
@@ -32,36 +32,37 @@ const colors = ["red", "green", "blue", "orange", "purple"]; // Puedes agregar m
 //       document.body.classList.add("dark-mode");
 //     }
 //   });
-  
+
 //   // Llamar a toggleDarkMode() cuando se haga clic en un botón o elemento de cambio
 //   document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
-  
 
-// Función para agregar el botón "Copy to Clipboard" automáticamente
+// Función para agregar el botón "Copy to Clipboard" en la parte superior del bloque de código
 function addCopyButtonsToCodeBlocks() {
-  const codeBlocks = document.querySelectorAll('pre code');
+  const codeBlocks = document.querySelectorAll("pre code");
   codeBlocks.forEach((codeBlock) => {
-    const copyButton = document.createElement('button');
+    const copyButton = document.createElement("button");
     copyButton.innerHTML = '<i class="fas fa-clipboard"></i>'; // Agrega el icono de Font Awesome
 
-    copyButton.classList.add('copy-button');
+    copyButton.classList.add("copy-button");
 
-    copyButton.addEventListener('click', () => {
-      const textArea = document.createElement('textarea');
+    copyButton.addEventListener("click", () => {
+      const textArea = document.createElement("textarea");
       textArea.value = codeBlock.textContent;
       document.body.appendChild(textArea);
       textArea.select();
-      document.execCommand('copy');
+      document.execCommand("copy");
       document.body.removeChild(textArea);
       copyButton.innerHTML = '<i class="fas fa-check"></i>'; // Cambia el icono a un icono de confirmación
       setTimeout(() => {
-        copyButton.innerHTML = '<i class="fas fa-clipboard"></i>'; // Restaura el icono "Copy to Clipboard" después de 2 segundos
+        copyButton.innerHTML =
+          '<i class="fas fa-clipboard"></i>'; // Restaura el icono "Copy to Clipboard" después de 2 segundos
       }, 2000);
     });
 
-    codeBlock.parentElement.appendChild(copyButton);
+    // Insertar el botón antes del bloque de código
+    codeBlock.parentElement.insertBefore(copyButton, codeBlock);
   });
 }
 
 // Llamar a la función para agregar los botones después de que la página se cargue
-window.addEventListener('DOMContentLoaded', addCopyButtonsToCodeBlocks);
+window.addEventListener("DOMContentLoaded", addCopyButtonsToCodeBlocks);
